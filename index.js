4130a196cddec3fd67a0fa2634e828ccc57f6d8c
hilarious-programmer-2558 <combatdstar@gmail.com>
2019-08-17 11:16:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ function sayHiToGrandma(string) {
   if (string == 'I love you, Grandma.'){
     console.log('I love you, too.')
   } else if (string == shout(string)) {
-    console.log('YES INDEED!')
+    return 'YES INDEED!'
+  } else if (string == whisper(string)) {
+    return 'I can\'t hear you!'
   }
 }
